d270458c839e6ff769c2ddd15893af40fe12e607
ZaminMirzad <mohzamin89@gmail.com>
2022-08-27 11:48:23 +0300

    timestamps update

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -3,12 +3,15 @@ const Double = require('@mongoosejs/double');
 
 const { Schema } = mongoose;
 
-const cartSchema = new Schema({
-  userId: String,
-  cartTotalPrice: Double,
-  cartTotalItems: Number,
-  items: Array,
-});
+const cartSchema = new Schema(
+  {
+    userId: String,
+    cartTotalPrice: Double,
+    cartTotalItems: Number,
+    items: Array,
+  },
+  { timestamps: true }
+);
 
 const Cart = mongoose.model('Cart', cartSchema);
 module.exports = { Cart };
--- src/models/Product.js
+++ src/models/Product.js
@@ -3,18 +3,21 @@ const Double = require('@mongoosejs/double');
 
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
-  id: String,
-  title: String,
-  description: String,
-  category: String,
-  image: String,
-  price: Double,
-  rating: {
-    rate: Double,
-    count: Number,
+const productSchema = new Schema(
+  {
+    id: String,
+    title: String,
+    description: String,
+    category: String,
+    image: String,
+    price: Double,
+    rating: {
+      rate: Double,
+      count: Number,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const Product = mongoose.model('Product', productSchema);
 module.exports = { Product };
